5de78c4603f386cb8e007ed641d0fff2dfe08392
FunkyBookends <nonclercqc@gmail.com>
2016-11-26 10:52:02 +0000

    initial commit

---
+++ src/main/java/com/ignoretheextraclub/IgnoreTheExtraClubApplication.java
@@ -0,0 +1,12 @@
+package com.ignoretheextraclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoreTheExtraClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnoreTheExtraClubApplication.class, args);
+	}
+}
