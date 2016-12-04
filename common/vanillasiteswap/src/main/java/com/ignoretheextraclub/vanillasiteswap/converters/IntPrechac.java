18beec49182e7a69505780e6c1cb9f8a636d1da1
FunkyBookends <nonclercqc@gmail.com>
2016-12-04 10:30:34 +0000

    remove hard problem of parseing prechac, add method to conver int[]

--- common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/converters/IntPrechac.java
+++ common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/converters/IntPrechac.java
@@ -2,8 +2,10 @@ package com.ignoretheextraclub.vanillasiteswap.converters;
 
 import com.ignoretheextraclub.vanillasiteswap.exceptions.InvalidPrechacException;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Created by caspar on 01/12/16.
@@ -14,22 +16,15 @@ public class IntPrechac
     private static final char DELIMETER = ' ';
     private static final char DOT = '.';
 
-    private static final char[] VALID_DELIMETERS = new char[]{' ', ';', ','};
-    private static final Pattern PRECHAC_THROW_REGEX = Pattern.compile("^\\d+(\\.5[pP])?$");
-
     public static String intToPrechac(int thro)
     {
         if (thro % 2 == 0) return String.valueOf(thro/2);
         return String.valueOf(thro/2) + DOT + "5" + PASS;
     }
 
-    public static int prechacToInt(String thro) throws InvalidPrechacException
+    public static String intToPrechac(int[] thros)
     {
-        final Matcher matcher = PRECHAC_THROW_REGEX.matcher(thro);
-        final boolean matches = matcher.find();
-        if (matches && thro.length() == 1) return Integer.valueOf(thro) * 2;
-        else if (matches && thro.length() == 4) return Integer.valueOf(thro.substring(0, 3)) *2 ;
-        else throw new InvalidPrechacException("[" + thro + "] is not a valid prechac throw");
+        return Arrays.stream(thros).boxed().map(IntPrechac::intToPrechac).collect(Collectors.joining(String.valueOf(DELIMETER), "", ""));
     }
 
 }
