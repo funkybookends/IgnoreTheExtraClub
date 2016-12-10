a9b5bf38262d43560113e028b13507fb2efe2000
FunkyBookends <nonclercqc@gmail.com>
2016-12-10 20:23:28 +0000

    remove throwing of exception in favor of ignoring it

--- common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/converters/StringMultiplex.java
+++ common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/converters/StringMultiplex.java
@@ -1,7 +1,7 @@
 package com.ignoretheextraclub.vanillasiteswap.converters;
 
-import com.ignoretheextraclub.vanillasiteswap.state.MultiplexState;
-import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import com.ignoretheextraclub.vanillasiteswap.exceptions.BadThrowException;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -43,12 +43,20 @@ public class StringMultiplex
         return allThros;
     }
 
-    public static String throwsToString(final List<List<Integer>> allThros) throws InvalidArgumentException
+    /**
+     * Converts a list of list of throws to a string.
+     *
+     * Silently ignores empty lists.
+     *
+     * @param allThros
+     * @return the string representation of the throws
+     */
+    public static String throwsToString(final List<List<Integer>> allThros)
     {
         StringBuilder str = new StringBuilder();
         for (List<Integer> beatThros : allThros)
         {
-            if      (beatThros.size() == 0) throw new InvalidArgumentException(new String[]{"Cannot have no throws in a beat"});
+            if      (beatThros.size() == 0) {} // Silently ignore error
             else if (beatThros.size() == 1) str.append(beatThros.get(0));
             else
             {
