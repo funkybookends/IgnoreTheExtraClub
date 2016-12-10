00b6d34937e121e0dec6500be85245bcf4e2c813
FunkyBookends <nonclercqc@gmail.com>
2016-12-10 11:08:24 +0000

    refactor to have builder classes to create siteswaps

--- common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/siteswap/FourHandedSiteswap.java
+++ common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/siteswap/FourHandedSiteswap.java
@@ -1,6 +1,7 @@
 package com.ignoretheextraclub.vanillasiteswap.siteswap;
 
 import com.ignoretheextraclub.vanillasiteswap.converters.GlobalLocal;
+import com.ignoretheextraclub.vanillasiteswap.converters.IntPrechac;
 import com.ignoretheextraclub.vanillasiteswap.converters.IntVanilla;
 import com.ignoretheextraclub.vanillasiteswap.exceptions.InvalidSiteswapException;
 import jdk.nashorn.internal.ir.annotations.Immutable;
@@ -14,19 +15,64 @@ public class FourHandedSiteswap extends VanillaSiteswap
     private final static int[] ILLEGAL_THROWS = new int[]{1,3};
     private static final int MAX_THROW = 12; //C
 
+    private final int[] leaderIntSiteswap;
+    private final int[] followerIntSiteswap;
+    private final String leaderStringSiteswap;
+    private final String followerStringSiteswap;
+    private final String leaderPrechac;
+    private final String followerPrechac;
+    private final String prechac;
+
+    public FourHandedSiteswap(boolean prime,
+                              int period,
+                              int numObjects,
+                              boolean grounded,
+                              int highestThrow,
+                              boolean sorted,
+                              String stringSiteswap,
+                              int[] intSiteswap,
+                              int[] startingObjectsPerHand,
+                              int[] leaderIntSiteswap,
+                              int[] followerIntSiteswap,
+                              String leaderStringSiteswap,
+                              String followerStringSiteswap,
+                              String leaderPrechac,
+                              String followerPrechac,
+                              String prechac)
+    {
+        super(prime,
+              period,
+              numObjects,
+              grounded,
+              highestThrow,
+              sorted,
+              stringSiteswap,
+              intSiteswap,
+              startingObjectsPerHand);
+        this.leaderIntSiteswap = leaderIntSiteswap;
+        this.followerIntSiteswap = followerIntSiteswap;
+        this.leaderStringSiteswap = leaderStringSiteswap;
+        this.followerStringSiteswap = followerStringSiteswap;
+        this.leaderPrechac = leaderPrechac;
+        this.followerPrechac = followerPrechac;
+        this.prechac = prechac;
+    }
+
     private static FourHandedSiteswap createGlobalOrLocal(int[] siteswap, boolean sort) throws InvalidSiteswapException
     {
         try
         {
-            return new FourHandedSiteswap(siteswap, sort);
+            FourHandedSiteswapBuilder builder = new FourHandedSiteswapBuilder(siteswap, sort);
+            return builder.buildFourHandedSiteswap();
         }
-        catch (InvalidSiteswapException e)
+        catch (InvalidSiteswapException invalidAsGlobal)
         {
             try
             {
-                return new FourHandedSiteswap(GlobalLocal.localToGlobal(siteswap), sort);
+                FourHandedSiteswapBuilder builder = new FourHandedSiteswapBuilder(GlobalLocal.localToGlobal(siteswap), sort);
+                return builder.buildFourHandedSiteswap();
             }
-            catch (InvalidSiteswapException e1)
+            catch (InvalidSiteswapException invalidAsLocal)
             {
                 throw new InvalidSiteswapException("Invalid as either Global or Local Siteswap");
             }
@@ -53,29 +99,58 @@ public class FourHandedSiteswap extends VanillaSiteswap
         return FourHandedSiteswap.createGlobalOrLocal(IntVanilla.stringToIntArray(siteswap), true);
     }
 
-    private FourHandedSiteswap(int[] vanillaSiteswap, boolean sort) throws InvalidSiteswapException
+    protected static class FourHandedSiteswapBuilder extends VanillaSiteswapBuilder
     {
-        super(vanillaSiteswap, sort);
-        if (this.getHighestThrow() > MAX_THROW) throw new InvalidSiteswapException("Four Handed Siteswaps Cannot have throws larger than " + MAX_THROW);
-        for (int thro : vanillaSiteswap)
+        private int[] leaderIntSiteswap;
+        private int[] followerIntSiteswap;
+        private String leaderStringSiteswap;
+        private String followerStringSiteswap;
+        private String leaderPrechac;
+        private String followerPrechac;
+        private String prechac;
+
+        public FourHandedSiteswapBuilder(int[] vanillaSiteswap, boolean sort) throws InvalidSiteswapException
         {
-            for (int illegalThrow : ILLEGAL_THROWS)
+            super(vanillaSiteswap, sort, 4);
+
+            if (this.highestThrow > MAX_THROW) throw new InvalidSiteswapException("Four Handed Siteswaps Cannot have throws larger than " + MAX_THROW);
+            for (int thro : vanillaSiteswap)
             {
-                if (thro == illegalThrow) throw new InvalidSiteswapException("Throw [" + thro + "] is illegal in Four Handed Siteswaps");
+                for (int illegalThrow : ILLEGAL_THROWS)
+                {
+                    if (thro == illegalThrow) throw new InvalidSiteswapException("Throw [" + thro + "] is illegal in Four Handed Siteswaps");
+                }
             }
+
+            leaderIntSiteswap = GlobalLocal.globalToLocal(this.intSiteswap, 0);
+            leaderStringSiteswap = IntVanilla.intArrayToString(leaderIntSiteswap);
+            followerIntSiteswap = GlobalLocal.globalToLocal(this.intSiteswap, 1);
+            followerStringSiteswap = IntVanilla.intArrayToString(followerIntSiteswap);
+            prechac = IntPrechac.intToPrechac(intSiteswap);
+            leaderPrechac = IntPrechac.intToPrechac(leaderIntSiteswap);
+            followerPrechac = IntPrechac.intToPrechac(followerIntSiteswap);
+
         }
-    }
 
-    public String localLeader()
-    {
-        final int[] localLeaderIntSiteswap = GlobalLocal.globalToLocal(this.intSiteswap, 0);
-        return VanillaSiteswap.toString(localLeaderIntSiteswap);
-    }
-
-    public String localFollower()
-    {
-        final int[] localFollowerIntSiteswap = GlobalLocal.globalToLocal(this.intSiteswap, 1);
-        return VanillaSiteswap.toString(localFollowerIntSiteswap);
+        protected FourHandedSiteswap buildFourHandedSiteswap()
+        {
+            return new FourHandedSiteswap(prime,
+                                          period,
+                                          numObjects,
+                                          grounded,
+                                          highestThrow,
+                                          sorted,
+                                          stringSiteswap,
+                                          intSiteswap,
+                                          startingObjectsPerHand,
+                                          leaderIntSiteswap,
+                                          followerIntSiteswap,
+                                          leaderStringSiteswap,
+                                          followerStringSiteswap,
+                                          leaderPrechac,
+                                          followerPrechac,
+                                          prechac);
+        }
     }
 
 }
--- common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/siteswap/VanillaSiteswap.java
+++ common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/siteswap/VanillaSiteswap.java
@@ -12,7 +12,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static com.ignoretheextraclub.vanillasiteswap.state.VanillaState.VanillaStateBuilder;
 import static com.ignoretheextraclub.vanillasiteswap.state.VanillaState.transition;
 
 /**
@@ -28,156 +27,59 @@ import static com.ignoretheextraclub.vanillasiteswap.state.VanillaState.transiti
 @Immutable
 public class VanillaSiteswap
 {
-    private static final int MAX_PERIOD = 15;
+    public static final int MAX_PERIOD = 15;
 
-    /**
-     * The list of states, ordered, and possibly sorted
-     */
-    private final List<VanillaState> states = new LinkedList<>();
-
-    /**
-     * A pattern is prime if it does not revisit a state twice.
-     *
-     * If it does this implies it can be decomposed into two or more siteswaps TODO determine decompositions
-     */
-    private final boolean prime;
-
-    private final int period;
-    private final int numObjects;
-
-    /**
-     * A pattern is grounded if it goes through the ground state, the ground state is the lowest level of excitiation
-     */
-    private final boolean grounded;
-
-    private final int highestThrow;
-    private final boolean sorted;
-    private final String stringSiteswap;
+    protected final List<VanillaState> states = new LinkedList<>(); //The list of states, ordered, and possibly sorted
+    protected final boolean prime; //A pattern is prime if it does not revisit a state twice. If it does this implies it can be decomposed into two or more siteswaps TODO determine decompositions
+    protected final int period;
+    protected final int numObjects;
+    protected final boolean grounded; //A pattern is grounded if it goes through the ground state, the ground state is the lowest level of excitiation
+    protected final int highestThrow;
+    protected final boolean sorted;
+    protected final String stringSiteswap;
     protected final int[] intSiteswap;
+    protected final int[] startingObjectsPerHand;
 
-    public static VanillaSiteswap create(int[] siteswap, boolean sort) throws InvalidSiteswapException
+    public VanillaSiteswap(boolean prime,
+                           int period,
+                           int numObjects,
+                           boolean grounded,
+                           int highestThrow,
+                           boolean sorted,
+                           String stringSiteswap,
+                           int[] intSiteswap,
+                           int[] startingObjectsPerHand)
     {
-        return new VanillaSiteswap(siteswap, sort);
+        this.prime = prime;
+        this.period = period;
+        this.numObjects = numObjects;
+        this.grounded = grounded;
+        this.highestThrow = highestThrow;
+        this.sorted = sorted;
+        this.stringSiteswap = stringSiteswap;
+        this.intSiteswap = intSiteswap;
+        this.startingObjectsPerHand = startingObjectsPerHand;
     }
 
-    public static VanillaSiteswap create(int[] siteswap) throws InvalidSiteswapException
+    public static VanillaSiteswap create(final int[] siteswap, final boolean sort) throws InvalidSiteswapException
     {
-        return new VanillaSiteswap(siteswap, true);
+        VanillaSiteswapBuilder builder = new VanillaSiteswapBuilder(siteswap, sort);
+        return builder.buildVanillaSiteswap();
     }
 
-    public static VanillaSiteswap create(String siteswap, boolean sort) throws InvalidSiteswapException
+    public static VanillaSiteswap create(final int[] siteswap) throws InvalidSiteswapException
     {
-        return new VanillaSiteswap(IntVanilla.stringToIntArray(siteswap), sort);
+        return create(siteswap, true);
     }
 
-    public static VanillaSiteswap create(String siteswap) throws InvalidSiteswapException
+    public static VanillaSiteswap create(final String siteswap, final boolean sort) throws InvalidSiteswapException
     {
-        final int[] intSiteswap = IntVanilla.stringToIntArray(siteswap);
-        return new VanillaSiteswap(intSiteswap, true);
+        return create(IntVanilla.stringToIntArray(siteswap), sort);
     }
 
-    protected VanillaSiteswap(int[] vanillaSiteswap, boolean sort) throws InvalidSiteswapException
+    public static VanillaSiteswap create(final String siteswap) throws InvalidSiteswapException
     {
-        if (vanillaSiteswap.length > MAX_PERIOD || vanillaSiteswap.length < 1)
-        {
-            throw new InvalidSiteswapException("Invalid siteswap length");
-        }
-        this.highestThrow = Arrays.stream(vanillaSiteswap).max().orElse(0);
-        this.numObjects = (int) Arrays.stream(vanillaSiteswap).average().orElse(0);
-        this.period = vanillaSiteswap.length;
-        try
-        {
-            buildStates(vanillaSiteswap);
-            this.grounded = containsAGroundState(states);
-            this.prime = !containsARepeatedState(); // both done here because they can be used during sorting
-            if (sort) sort();
-            this.sorted = sort;
-            this.intSiteswap = getAllThrows();
-            this.stringSiteswap = toString(this.intSiteswap);
-        }
-        catch (BadThrowException | NumObjectsException | StateSizeException | NoTransitionException e)
-        {
-            throw new InvalidSiteswapException("Invalid Siteswap [" + toString(vanillaSiteswap) + "]", e);
-        }
-
-        if (states.size() != period)
-        {
-            throw new RuntimeException("This shouldn't happen. Built states.size() != vanillaSiteswap.length");
-        }
-    }
-
-    private int[] getAllThrows() throws NoTransitionException
-    {
-        final int[] thros = new int[period];
-        for (int i = 0; i < period - 1; i++)
-        {
-            thros[i] = transition(states.get(i), states.get(i+1));
-        }
-        thros[period - 1] = transition(states.get(period - 1), states.get(0));
-        return thros;
-    }
-
-    private boolean containsARepeatedState()
-    {
-        Set<VanillaState> stateSet = states.stream().collect(Collectors.toSet());
-        return stateSet.size() != states.size();
-    }
-
-    private void buildStates(int[] vanillaSiteswap) throws NumObjectsException, BadThrowException, StateSizeException, InvalidSiteswapException
-    {
-        final VanillaStateBuilder builder = new VanillaStateBuilder(this.highestThrow, numObjects);
-        int index = 0;
-        while (builder.getGivenObjects() < numObjects || index % period != 0)
-        {
-            builder.thenThrow(vanillaSiteswap[index % period]);
-            index++;
-        }
-
-        VanillaState state = builder.build();
-        states.add(state);
-
-        for (int pos = 0; pos < vanillaSiteswap.length - 1; pos++)
-        {
-            state = state.thro(vanillaSiteswap[pos]);
-            states.add(state);
-        }
-        if (!state.thro(vanillaSiteswap[period - 1]).equals(states.get(0)))
-        {
-            throw new InvalidSiteswapException("Did not return to original state. Final state: [" + states.get(period-1) + "], original state: [" + states.get(0) + "]");
-        }
-    }
-
-    /**
-     * Sorts the siteswap, finds the groundiest first throw and makes that the first throw.
-     */
-    private void sort()
-    {
-        List<Integer> scores = IntStream.range(0, period).map(this::scoreRotation).boxed().collect(Collectors.toList());
-        int minPosition = IntStream.range(0, period).reduce((bestP, candP) -> scores.get(bestP) < scores.get(candP) ? bestP : candP).getAsInt();
-        while (minPosition > 0)
-        {
-            states.add(states.remove(0));
-            minPosition--;
-        }
-    }
-
-    private int scoreRotation(int start)
-    {
-        int score = 0;
-        for (int i = 0; i < period; i++)
-        {
-            int stateNumber = (start + i) % period;
-            VanillaState state = states.get(stateNumber);
-            int excitedness = state.excitedness();
-            score += excitedness*(-i+1);
-        }
-        return score;
-    }
-
-    private static boolean containsAGroundState(List<VanillaState> states)
-    {
-        for (VanillaState state : states) if (state.isGround()) return true;
-        return false;
+        return create(siteswap, true);
     }
 
     public boolean isPrime()
@@ -210,21 +112,191 @@ public class VanillaSiteswap
         return sorted;
     }
 
-    /**
-     * Converts an int[] to a string, guaranteed to never throw an exception - replaces any unknowns with "-"
-     * @param intSiteswap
-     * @return String representation of intSiteswap
-     */
-    protected static String toString(int[] intSiteswap)
-    {
-        return new String(IntVanilla.intArrayToCharArray(intSiteswap));
-    }
-
     @Override
     public String toString()
     {
         return this.stringSiteswap;
     }
 
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VanillaSiteswap that = (VanillaSiteswap) o;
+
+        return stringSiteswap.equals(that.stringSiteswap);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return stringSiteswap.hashCode();
+    }
+
+    /**
+     * A builder class that calculates all the values.
+     */
+    protected static class VanillaSiteswapBuilder
+    {
+        protected final List<VanillaState> states;
+        protected boolean prime;
+        protected int period;
+        protected int numObjects;
+        protected boolean grounded;
+        protected int highestThrow;
+        protected boolean sorted;
+        protected String stringSiteswap;
+        protected int[] intSiteswap;
+        protected int[] startingObjectsPerHand;
+
+        public VanillaSiteswapBuilder(final int[] vanillaSiteswap, final boolean sort, final int hands) throws
+                                                                                                        InvalidSiteswapException
+        {
+            if (vanillaSiteswap.length > VanillaSiteswap.MAX_PERIOD || vanillaSiteswap.length < 1)
+            {
+                throw new InvalidSiteswapException("Invalid siteswap length");
+            }
+            highestThrow = Arrays.stream(vanillaSiteswap).max().orElse(0);
+            numObjects = (int) Arrays.stream(vanillaSiteswap).average().orElse(0);
+            period = vanillaSiteswap.length;
+            states = new LinkedList<>();
+            startingObjectsPerHand = new int[hands];
+            try
+            {
+                buildStatesAndHands(vanillaSiteswap);
+                grounded = containsAGroundState();
+                prime = !containsARepeatedState(); // both done here because they can be used during sorting
+                if (sort) sort();
+                sorted = sort;
+                intSiteswap = getAllThrows();
+                stringSiteswap = IntVanilla.intArrayToString(intSiteswap);
+            }
+            catch (BadThrowException | NumObjectsException | StateSizeException | NoTransitionException e)
+            {
+                throw new InvalidSiteswapException("Invalid Siteswap [" + IntVanilla.intArrayToString(vanillaSiteswap) + "]", e);
+            }
+
+            if (states.size() != period)
+            {
+                throw new RuntimeException("This shouldn't happen. Built states.size() != vanillaSiteswap.length");
+            }
+        }
+
+        public VanillaSiteswapBuilder(final int[] vanillaSiteswap, final boolean sort) throws InvalidSiteswapException
+        {
+            this(vanillaSiteswap, sort, 2);
+        }
+
+        private int[] getAllThrows() throws NoTransitionException
+        {
+            final int[] thros = new int[period];
+            for (int i = 0; i < period - 1; i++)
+            {
+                thros[i] = transition(states.get(i), states.get(i+1));
+            }
+            thros[period - 1] = transition(states.get(period - 1), states.get(0));
+            return thros;
+        }
+
+        private boolean containsARepeatedState()
+        {
+            Set<VanillaState> stateSet = states.stream().collect(Collectors.toSet());
+            return stateSet.size() != states.size();
+        }
+
+        private void buildStatesAndHands(int[] vanillaSiteswap) throws NumObjectsException,
+                                                                       BadThrowException,
+                                                                       StateSizeException,
+                                                                       InvalidSiteswapException
+        {
+            final VanillaState.VanillaStateBuilder builder = new VanillaState.VanillaStateBuilder(this.highestThrow, numObjects);
+            int index = 0;
+            int givenObjects = 0;
+            while (builder.getGivenObjects() < numObjects || index % period != 0)
+            {
+                builder.thenThrow(vanillaSiteswap[index % period]);
+                if (givenObjects < builder.getGivenObjects())
+                {
+                    givenObjects++;
+                    startingObjectsPerHand[index % startingObjectsPerHand.length]++;
+                }
+                index++;
+            }
+
+            VanillaState state = builder.build();
+            states.add(state);
+
+            for (int pos = 0; pos < vanillaSiteswap.length - 1; pos++)
+            {
+                state = state.thro(vanillaSiteswap[pos]);
+                states.add(state);
+            }
+            if (Arrays.stream(startingObjectsPerHand).sum() != states.get(0).getNumObjects())
+            {
+                throw new RuntimeException("Did not calculate the number of objects in each hand correctly. " +
+                                                   "sum(" + startingObjectsPerHand+ ") != " + states.get(0).getNumObjects());
+            }
+            if (!state.thro(vanillaSiteswap[period - 1]).equals(states.get(0)))
+            {
+                throw new InvalidSiteswapException("Did not return to original state. " +
+                                                           "Final state: [" + states.get(period-1) + "], original state: [" + states.get(0) + "]");
+            }
+        }
+
+        /**
+         * Sorts the siteswap, finds the groundiest first throw and makes that the first throw.
+         */
+        private void sort()
+        {
+            List<Integer> scores = IntStream.range(0, period)
+                    .map(this::scoreRotation)
+                    .boxed()
+                    .collect(Collectors.toList());
+
+            int minPosition = IntStream.range(0, period)
+                    .reduce((bestP, candP) -> scores.get(bestP) < scores.get(candP) ? bestP : candP)
+                    .getAsInt();
+
+            while (minPosition > 0)
+            {
+                states.add(states.remove(0));
+                minPosition--;
+            }
+        }
+
+        private int scoreRotation(int start)
+        {
+            int score = 0;
+            for (int i = 0; i < period; i++)
+            {
+                int stateNumber = (start + i) % period;
+                VanillaState state = states.get(stateNumber);
+                int excitedness = state.excitedness();
+                score += excitedness*(-i+1);
+            }
+            return score;
+        }
+
+        private boolean containsAGroundState()
+        {
+            for (VanillaState state : states) if (state.isGround()) return true;
+            return false;
+        }
+
+        private VanillaSiteswap buildVanillaSiteswap()
+        {
+            return new VanillaSiteswap(prime,
+                                       period,
+                                       numObjects,
+                                       grounded,
+                                       highestThrow,
+                                       sorted,
+                                       stringSiteswap,
+                                       intSiteswap,
+                                       startingObjectsPerHand);
+        }
+    }
 
 }
