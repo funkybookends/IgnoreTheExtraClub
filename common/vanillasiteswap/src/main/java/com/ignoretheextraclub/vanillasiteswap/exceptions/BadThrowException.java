f3e21525eb97466542e88ad89287bb3c69533907
FunkyBookends <nonclercqc@gmail.com>
2016-12-10 14:23:00 +0000

    remove typo

--- common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/exceptions/BadThrowException.java
+++ common/vanillasiteswap/src/main/java/com/ignoretheextraclub/vanillasiteswap/exceptions/BadThrowException.java
@@ -4,7 +4,7 @@ package com.ignoretheextraclub.vanillasiteswap.exceptions;
  * Created by caspar on 26/11/16.
  */
 public class BadThrowException extends Throwable
-1{
+{
     public BadThrowException(String message) {
         super(message);
     }
