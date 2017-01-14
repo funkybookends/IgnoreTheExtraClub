937e7c565f87c70c3d4c4608bbc20042b17e6e53
FunkyBookends <nonclercqc@gmail.com>
2017-01-14 13:15:14 +0000

    move to rest package

--- src/main/java/com/ignoretheextraclub/controllers/rest/RESTSiteswap.java
+++ src/main/java/com/ignoretheextraclub/controllers/rest/RESTSiteswap.java
@@ -1,7 +1,8 @@
-package com.ignoretheextraclub.controllers;
+package com.ignoretheextraclub.controllers.rest;
 
 import com.ignoretheextraclub.siteswapfactory.exceptions.InvalidSiteswapException;
 import com.ignoretheextraclub.siteswapfactory.siteswap.vanilla.FourHandedSiteswap;
+import com.ignoretheextraclub.siteswapfactory.siteswap.vanilla.TwoHandedSiteswap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by caspar on 14/01/17.
  */
 @RestController
+@RequestMapping("/rest/v1")
 public class RESTSiteswap
 {
     private static final Logger LOG = LoggerFactory.getLogger(RESTSiteswap.class);
 
-    @RequestMapping(path = "rest/v1/p/fhs/{siteswap}", method = RequestMethod.GET)
+    @RequestMapping(path = "/p/fhs/{siteswap}", method = RequestMethod.GET)
     public FourHandedSiteswap fhsController(@PathVariable(value = "siteswap") final String siteswap) throws InvalidSiteswapException
     {
-        LOG.info("request for: {}", siteswap);
-        final FourHandedSiteswap fourHandedSiteswap = FourHandedSiteswap.create(siteswap);
-        return fourHandedSiteswap;
+        LOG.info("Request for FHS: {}", siteswap);
+        return FourHandedSiteswap.create(siteswap);
+    }
+
+    @RequestMapping(path = "/p/ths/{siteswap}", method = RequestMethod.GET)
+    public TwoHandedSiteswap thsController(@PathVariable(value = "siteswap") final String siteswap) throws InvalidSiteswapException
+    {
+        LOG.info("Request for THS: {}", siteswap);
+        return TwoHandedSiteswap.create(siteswap);
     }
 
 }
--- src/test/java/com/ignoretheextraclub/controllers/rest/RESTSiteswapTest.java
+++ src/test/java/com/ignoretheextraclub/controllers/rest/RESTSiteswapTest.java
@@ -1,4 +1,4 @@
-package com.ignoretheextraclub.controllers;
+package com.ignoretheextraclub.controllers.rest;
 
 import org.junit.Before;
 import org.junit.Test;
