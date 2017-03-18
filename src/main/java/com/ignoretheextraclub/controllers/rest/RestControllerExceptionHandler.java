4ad602516084a0a74affde198b02992fa408c29d
FunkyBookends <nonclercqc@gmail.com>
2017-03-18 10:49:07 +0000

    move exception handler and change to rest

--- src/main/java/com/ignoretheextraclub/controllers/rest/RestControllerExceptionHandler.java
+++ src/main/java/com/ignoretheextraclub/controllers/rest/RestControllerExceptionHandler.java
@@ -1,17 +1,17 @@
-package com.ignoretheextraclub.controllers.advice;
+package com.ignoretheextraclub.controllers.rest;
 
 import com.ignoretheextraclub.exceptions.UnknownPatternException;
 import com.ignoretheextraclub.siteswapfactory.exceptions.InvalidSiteswapException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * Created by caspar on 12/03/17.
  */
-@ControllerAdvice
-public class GlobalControllerExceptionHandler
+@RestControllerAdvice
+public class RestControllerExceptionHandler
 {
     @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED, reason =
             "Could not find a pattern with that name or construct a siteswap with it.")
