a6d155cc154720c8e2ef78b93e18646a8ebc9370
FunkyBookends <nonclercqc@gmail.com>
2017-04-02 12:59:16 +0100

    remove dependancy on mongo for unit tests

--- src/test/java/com/ignoretheextraclub/service/impl/PatternServiceImplTest.java
+++ src/test/java/com/ignoretheextraclub/service/impl/PatternServiceImplTest.java
@@ -13,18 +13,28 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by caspar on 08/03/17.
@@ -37,7 +47,7 @@ public class PatternServiceImplTest
     // Service under test
     private PatternService patternService;
 
-    private @Autowired PatternRepository patternRepository;
+    private @MockBean PatternRepository patternRepository;
 
     // Mocks
     private PatternConstructor pc1;
@@ -56,67 +66,75 @@ public class PatternServiceImplTest
     @Test
     public void GIVEN_noPattern_EXPECT_empty() throws Exception
     {
-        Assert.assertFalse(patternService.get("any name").isPresent());
+        when(patternRepository.findByName("any name")).thenReturn(Optional.empty());
+        assertThat(patternService.get("any name")).isNotPresent();
     }
 
     @Test
     public void GIVEN_pattern_EXPECT_pattern() throws Exception
     {
         final String name = "name";
-        final Pattern pattern = new Pattern(FourHandedSiteswap.create("975"),
-                                            new PatternName(name, 0));
-        patternRepository.save(pattern);
-        Assert.assertTrue(patternService.get(name).isPresent());
+        final Pattern pattern = new Pattern(FourHandedSiteswap.create("975"), new PatternName(name, 0));
+
+        when(patternRepository.findByName(name)).thenReturn(Optional.of(pattern));
+        assertThat(patternService.get(name)).isPresent();
+        verify(patternRepository, times(1)).findByName(name);
     }
 
     @Test
     public void GIVEN_pc1CantConstruct_EXPECT_pc2IsCalled_AND_patternIsSaved() throws Exception
     {
         String name = "345";
+
+        when(patternRepository.findByName(name)).thenReturn(Optional.empty());
+        when(patternRepository.findByName("534")).thenReturn(Optional.empty());
+        when(patternRepository.save(any(Pattern.class))).then(returnsFirstArg());
+
         final Pattern actual = patternService.getOrCreate(name);
 
         verify(pc1, times(1)).getNaturalName(name);
         verify(pc1, times(0)).createPattern(name);
         verify(pc2, times(1)).getNaturalName(name);
         verify(pc2, times(1)).createPattern(name);
+
+        verify(patternRepository, times(1)).save(actual);
     }
 
     @Test
     public void GIVEN_save_EXPECT_saved() throws Exception
     {
-        Assert.assertFalse(patternRepository.findByName("name").isPresent());
+        when(patternRepository.save(any(Pattern.class))).then(returnsFirstArg());
 
-        final Pattern pattern = new Pattern(FourHandedSiteswap.create("975"),
-                                      new PatternName("name", 0));
+        final Pattern pattern = new Pattern(FourHandedSiteswap.create("975"), new PatternName("name", 0));
 
-        patternService.save(pattern);
+        final Pattern save = patternService.save(pattern);
 
-        Assert.assertTrue(patternRepository.findByName("name").isPresent());
-        Assert.assertTrue(patternService.get("name").isPresent());
+        verify(patternRepository, times(1)).save(pattern);
+        assertThat(save).isEqualTo(pattern);
     }
 
     @Test
     public void GIVEN_newerPatterns_EXPECT_pagesCorrectly() throws Exception
     {
-        final String[] siteswaps = {"975", "678", "9A8", "756", "78686", "7868686"};
+        final Page mockPage = mock(Page.class);
 
-        for (String siteswap : siteswaps)
-        {
-            patternService.getOrCreate(siteswap);
-        }
+        final ArgumentCaptor<PageRequest> pageRequestArgumentCaptor = ArgumentCaptor.forClass(PageRequest.class);
 
-        Page<Pattern> newest = patternService.newest(1, 2);
+        when(patternRepository.findAll(any(PageRequest.class))).thenReturn(mockPage);
 
-        long totalElements = newest.getTotalElements();
-        Assert.assertEquals(siteswaps.length, totalElements);
+        final int page = 1;
+        final int size = 2;
 
-        Assert.assertEquals(3,newest.getTotalPages());
+        Page<Pattern> newest = patternService.newest(page, size);
 
-        ArrayList<Pattern> patterns = new ArrayList<>();
-        newest.forEach(patterns::add);
+        verify(patternRepository, times(1)).findAll(pageRequestArgumentCaptor.capture());
 
-        Assert.assertEquals(2, patterns.size());
-        Assert.assertEquals("756", patterns.get(0).getSiteswap().toString());
-        Assert.assertEquals("9A8", patterns.get(1).getSiteswap().toString());
+        final PageRequest pageRequest = pageRequestArgumentCaptor.getValue();
+
+        assertThat(pageRequest).hasFieldOrPropertyWithValue("page", page);
+        assertThat(pageRequest).hasFieldOrPropertyWithValue("size", size);
+        final Sort sort = pageRequest.getSort();
+        assertThat(sort.getOrderFor("createdDate").getDirection()).isEqualTo(Sort.Direction.DESC);
+        assertThat(newest).isEqualTo(mockPage);
     }
 }
