542365f15299d952b1d8290e150d7db09c9b24fb
FunkyBookends <nonclercqc@gmail.com>
2017-04-02 13:32:10 +0100

    add properties and configuration

--- src/main/java/com/ignoretheextraclub/configuration/MetricsConfiguration.java
+++ src/main/java/com/ignoretheextraclub/configuration/MetricsConfiguration.java
@@ -3,9 +3,11 @@ package com.ignoretheextraclub.configuration;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
+import com.ignoretheextraclub.properties.GraphiteProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,7 @@ import java.util.concurrent.TimeUnit;
  * Created by caspar on 19/03/17.
  */
 @Configuration
+@EnableConfigurationProperties(GraphiteProperties.class)
 public class MetricsConfiguration
 {
     private static final Logger LOG = LoggerFactory.getLogger(MetricsConfiguration.class);
@@ -25,19 +28,27 @@ public class MetricsConfiguration
         return new MetricRegistry();
     }
 
-    @Autowired(required = false)
-    private void configureGraphiteReporter(final MetricRegistry metricRegistry)
+    @Autowired
+    private void configureGraphiteReporter(final MetricRegistry metricRegistry,
+            final GraphiteProperties graphiteProperties)
     {
-        // TODO get all these props in configuration file and log them out
-        LOG.info("Configuring Graphite.");
-        Graphite graphite = new Graphite("localhost", 2003);
+        if (graphiteProperties.isEnabled())
+        {
+            Graphite graphite = new Graphite(graphiteProperties.getHostname(),
+                    graphiteProperties.getPort());
 
-        GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry)
-                                                    .prefixedWith("IgnoreTheExtraClub")
-                                                    .convertDurationsTo(TimeUnit.MILLISECONDS)
-                                                    .convertRatesTo(TimeUnit.MILLISECONDS)
-                                                    .build(graphite);
+            GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry)
+                    .prefixedWith(graphiteProperties.getPrefix())
+                    .convertDurationsTo(TimeUnit.MILLISECONDS)
+                    .convertRatesTo(TimeUnit.MILLISECONDS)
+                    .build(graphite);
 
-        reporter.start(30, TimeUnit.SECONDS);
+            reporter.start(graphiteProperties.getPeriodSeconds(), TimeUnit.SECONDS);
+            LOG.info("Graphite configured and started: {}", graphiteProperties);
+        }
+        else
+        {
+            LOG.warn("Graphite is not enabled! {}", graphiteProperties);
+        }
     }
 }
